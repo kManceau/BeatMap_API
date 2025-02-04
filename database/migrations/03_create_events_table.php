84a440304486a207226c61019a4f1c99e8e030df
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-04 09:31:50 +0100

    Included ER Model in ReadMe

--- database/migrations/03_create_events_table.php
+++ database/migrations/03_create_events_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->text('description');
             $table->date('start_date');
             $table->date('end_date');
