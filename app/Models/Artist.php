55880414b2727dad0eb794f9874b088eaab8b54e
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-04 09:56:33 +0100

    Created Artist model

---
+++ app/Models/Artist.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'photo',
+        'style_id'
+    ];
+    public $timestamps = false;
+
+    public function style()
+    {
+        return $this->belongsTo(Style::class);
+    }
+}
