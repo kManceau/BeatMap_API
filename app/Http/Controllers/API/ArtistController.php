42032b1287b9235533ac1f59aacdd56253b373cf
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-04 10:13:56 +0100

    Created Controllers

---
+++ app/Http/Controllers/API/ArtistController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Artist;
+use Illuminate\Http\Request;
+
+class ArtistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Artist $artist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Artist $artist)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/API/StyleController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Style;
+use Illuminate\Http\Request;
+
+class StyleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Style $style)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Style $style)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Style $style)
+    {
+        //
+    }
+}
