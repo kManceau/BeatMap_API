307170ca124d83f2a7350e14c24c0939fdf25207
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-04 09:45:16 +0100

    Created Places migration

---
+++ database/migrations/03_create_places_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('places', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->string('country');
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->string('photo');
+            $table->foreignId('created_by')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('places');
+    }
+};
