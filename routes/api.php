2d0721d7993c5502dc06d5a893a61f4c84d23590
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-04 10:20:23 +0100

    Created getAll routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\API\ArtistController;
+use App\Http\Controllers\API\EventController;
+use App\Http\Controllers\API\PlaceController;
+use App\Http\Controllers\API\StyleController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Support\Facades\Route;
 
-
+// GET ALL Routes
+Route::get('/artists', [ArtistController::class, 'index'])->name('artists.all');
+Route::get('/events', [EventController::class, 'index'])->name('events.all');
+Route::get('/places', [PlaceController::class, 'index'])->name('places.all');
+Route::get('/styles', [StyleController::class, 'index'])->name('styles.all');
+Route::get('/users', [UserController::class, 'index'])->name('users.all');
