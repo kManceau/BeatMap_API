4a72fe0b220c5fefd3422bfe049755535e7e93d2
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-04 09:58:55 +0100

    Created Place model

---
+++ app/Models/Place.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Place extends Model
+{
+    use HasFactory;
+
+    public $timestamps = false;
+    protected $fillable = [
+        'name',
+        'description',
+        'address',
+        'city',
+        'state',
+        'country',
+        'zip',
+        'latitude',
+        'longitude',
+        'photo',
+        'created_by',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
