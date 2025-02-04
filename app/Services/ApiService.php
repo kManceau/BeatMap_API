2c80d2d2a916a86ce4b74346147de2bd1a4167e6
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-04 10:12:00 +0100

    Created getAll function

--- app/Services/ApiService.php
+++ app/Services/ApiService.php
@@ -2,7 +2,34 @@
 
 namespace App\Services;
 
+use App\Models\Artist;
+use App\Models\Event;
+use App\Models\Place;
+use App\Models\Style;
+use App\Models\User;
+
 class ApiService
 {
-    
+    public function getAll($model)
+    {
+        switch ($model) {
+            case 'events' :
+                $query = Event::all();
+                break;
+            case 'places' :
+                $query = Place::all();
+                break;
+            case 'users' :
+                $query = User::all();
+                break;
+            case 'styles' :
+                $query = Style::all();
+                break;
+            case 'artists' :
+                $query = Artist::all();
+                break;
+        }
+        $data = $query->get();
+        return response()->json($data);
+    }
 }
