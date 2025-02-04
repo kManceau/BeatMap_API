0f35b24aab89f8a0c825a54f6a5e4b64078f7364
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-04 09:03:50 +0100

    Created Artists migration

---
+++ database/migrations/02_create_artists_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('photo')->nullable();
+            $table->foreignId('style_id')->constrained('styles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artists');
+    }
+};
