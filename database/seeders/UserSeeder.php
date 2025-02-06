f4b3752e8667d80a52b1ae69246c63be690ed787
Kevin Manceau <kevin.manceau@gmail.com>
2025-02-06 09:23:21 +0100

    Updated User Seeder for photo

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,7 +20,7 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
             'role' => 'admin',
-            'photo' => null,
+            'photo' => 'kevinmanceau',
         ]);
         User::create([
             'id' => 2,
